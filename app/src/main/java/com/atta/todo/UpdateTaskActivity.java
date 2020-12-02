04d5800106b2c1e18286f2326e5ddba82a2e7e02
mostafa <mostafa.sa.atta@gmail.com>
2020-12-02 20:27:32 +0200

    delete task

--- app/src/main/java/com/atta/todo/UpdateTaskActivity.java
+++ app/src/main/java/com/atta/todo/UpdateTaskActivity.java
@@ -1,14 +1,72 @@
 package com.atta.todo;
 
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class UpdateTaskActivity extends AppCompatActivity {
+
+    Task task;
+
+    ImageView deleteTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_task);
+
+        task = (Task) getIntent().getSerializableExtra("task");
+
+        String taskName = task.getTask();
+
+
+
+        deleteTask = findViewById(R.id.imageView);
+        deleteTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteTask();
+            }
+        });
+
     }
+
+
+    private void deleteTask(){
+
+        class DeleteTask extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+            }
+
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                DatabaseClient.getInstance(UpdateTaskActivity.this)
+                        .getAppDatabase().taskDao().delete(task);
+
+                return null;
+            }
+
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent(UpdateTaskActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }
+
+        DeleteTask task = new DeleteTask();
+        task.execute();
+    }
+
 }
